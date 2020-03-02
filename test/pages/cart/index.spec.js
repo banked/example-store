1373983553b31f02b53b28704abf5d80897bda35
Joe Pettersson <joe8bit@gmail.com>
2020-03-02 14:04:56 +0000

    Add UI for adding/removing discount and test

--- test/pages/cart/index.spec.js
+++ test/pages/cart/index.spec.js
@@ -1,4 +1,5 @@
-import { mount } from '@vue/test-utils'
+import Vuex from 'vuex'
+import { mount, createLocalVue } from '@vue/test-utils'
 import axios from 'axios'
 import Masthead from '../../../components/Masthead.vue'
 import Item from '../../../components/Item.vue'
@@ -6,21 +7,34 @@ import Index from '../../../pages/cart/index.vue'
 
 jest.mock('axios')
 
-const storeMock = {
-  state: {
-    cart: {
-      list: [{
-        amount: 10
-      }, {
-        amount: 12.50
-      }, {
-        amount: 11
-      }]
-    }
-  }
-}
+const localVue = createLocalVue()
+localVue.use(Vuex)
 
 describe('Index', () => {
+  let store
+  let mutations
+
+  beforeEach(() => {
+    mutations = {
+      'cart/applyDiscount': jest.fn(),
+      'cart/removeDiscount': jest.fn()
+    }
+    store = new Vuex.Store({
+      mutations,
+      state: {
+        cart: {
+          list: [{
+            amount: 10
+          }, {
+            amount: 12.50
+          }, {
+            amount: 11
+          }]
+        }
+      }
+    })
+  })
+
   it('includes the correct sub-components', () => {
     expect(Index.components.Masthead).toEqual(Masthead)
     expect(Index.components.Item).toEqual(Item)
@@ -28,39 +42,36 @@ describe('Index', () => {
 
   it('renders the masthead', () => {
     const wrapper = mount(Index, {
-      mocks: {
-        $store: storeMock
-      },
       stubs: {
         Masthead: "<div class='masthead'></div>",
         Item: "<div class='Item'></div>"
-      }
+      },
+      store,
+      localVue
     })
     expect(wrapper.findAll('.masthead')).toHaveLength(1)
   })
 
   it('renders three cart items', () => {
     const wrapper = mount(Index, {
-      mocks: {
-        $store: storeMock
-      },
       stubs: {
         Masthead: "<div class='masthead'></div>",
         Item: "<div class='item'></div>"
-      }
+      },
+      store,
+      localVue
     })
     expect(wrapper.findAll('.item')).toHaveLength(3)
   })
 
   it('renders the cart total', () => {
     const wrapper = mount(Index, {
-      mocks: {
-        $store: storeMock
-      },
       stubs: {
         Masthead: "<div class='masthead'></div>",
         Item: "<div class='item'></div>"
-      }
+      },
+      store,
+      localVue
     })
     expect(wrapper.find('.cart-total').text().trim()).toEqual('£33.50')
   })
@@ -71,13 +82,12 @@ describe('Index', () => {
     axios.post.mockResolvedValue({ url: 'https://example.com/checkout/' })
 
     const wrapper = mount(Index, {
-      mocks: {
-        $store: storeMock
-      },
       stubs: {
         Masthead: "<div class='masthead'></div>",
         Item: "<div class='item'></div>"
-      }
+      },
+      store,
+      localVue
     })
 
     wrapper.find('button').trigger('click')
@@ -94,13 +104,12 @@ describe('Index', () => {
   it('should show an error when checkout request fails', (done) => {
     axios.post.mockRejectedValue(new Error('Roh roh shaggy'))
     const wrapper = mount(Index, {
-      mocks: {
-        $store: storeMock
-      },
       stubs: {
         Masthead: "<div class='masthead'></div>",
         Item: "<div class='item'></div>"
-      }
+      },
+      store,
+      localVue
     })
 
     wrapper.find('button').trigger('click')
@@ -115,21 +124,51 @@ describe('Index', () => {
 
   it('should show a message when a user has an empty cart', () => {
     const wrapper = mount(Index, {
-      mocks: {
-        $store: {
-          state: {
-            cart: {
-              list: []
-            }
-          }
-        }
-      },
       stubs: {
         Masthead: "<div class='masthead'></div>",
         Item: "<div class='item'></div>"
-      }
+      },
+      store: new Vuex.Store({
+        mutations,
+        state: {
+          cart: {
+            list: []
+          }
+        }
+      }),
+      localVue
     })
 
     expect(wrapper.find('.empty-cart').isVisible()).toBeTruthy()
+  })
+
+  it('apply and remove a discount when checkbox is clicked', (done) => {
+    const wrapper = mount(Index, {
+      stubs: {
+        Masthead: "<div class='masthead'></div>",
+        Item: "<div class='item'></div>"
+      },
+      store,
+      localVue
+    })
+    expect(wrapper.find('.cart-total').text().trim()).toEqual('£33.50')
+
+    const input = wrapper.find('.discount')
+
+    input.setChecked(true)
+
+    wrapper.vm.$nextTick(() => {
+      setTimeout(() => {
+        expect(mutations['cart/applyDiscount']).toBeCalled()
+        input.setChecked(false)
+
+        wrapper.vm.$nextTick(() => {
+          setTimeout(() => {
+            expect(mutations['cart/removeDiscount']).toBeCalled()
+            done()
+          }, 1)
+        })
+      }, 1)
+    })
   })
 })
